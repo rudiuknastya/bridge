6afe8fd84b537b284d7d7babcab48e3532870bcb
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-05-26 21:19:56 +0300

    Finished VillageMove class

--- VillagerMove.java
+++ VillagerMove.java
@@ -3,9 +3,31 @@ package bridge;
 public class VillagerMove implements Move{
     @Override
     public void move() {
-
+        bendDown();
+        takeSomething();
+        rise();
+        turn();
+        goForward();
+        putSomething();
+        turn();
+        goForward();
     }
-    private void goToTheLeft(){
-
+    private void bendDown(){
+        System.out.println("Bend down");
+    }
+    private void takeSomething(){
+        System.out.println("Take something");
+    }
+    private void rise(){
+        System.out.println("Rise");
+    }
+    private void turn(){
+        System.out.println("Turn");
+    }
+    private void goForward(){
+        System.out.println("Go forward");
+    }
+    private void putSomething(){
+        System.out.println("Put Something");
     }
 }
