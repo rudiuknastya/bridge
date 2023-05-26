d1adf198a9efc4057215bb0a76c38b95be26c730
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-05-26 21:25:23 +0300

    Finished Villager and Worker classes

--- Worker.java
+++ Worker.java
@@ -1,8 +1,14 @@
 package bridge;
 
 public class Worker extends Character{
+    Move move;
+
+    public Worker(Move move) {
+        this.move = move;
+    }
+
     @Override
     public void move() {
-
+        move.move();
     }
 }
